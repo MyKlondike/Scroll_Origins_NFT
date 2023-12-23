f9880b8204480dc4f18c250aa8de0bade4bdd5c1
Klondike <147417684+MyKlondike@users.noreply.github.com>
2023-12-23 12:59:08 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+import json
+
+with open('data/rpc.json') as file:
+    RPC = json.load(file)
+
+with open('data/abi/erc20_abi.json') as file:
+    ERC20_ABI = json.load(file)
+
+with open("accounts.txt", "r") as file:
+    ACCOUNTS = [row.strip() for row in file]
+
+with open('data/abi/scroll/weth.json') as file:
+    WETH_ABI = json.load(file)
+
+with open("data/abi/nft-origins/abi.json", "r") as file:
+    NFT_ORIGINS_ABI = json.load(file)
+
+NFT_ORIGINS_CONTRACT = "0x74670A3998d9d6622E32D0847fF5977c37E0eC91"
+
+SCROLL_TOKENS = {
+    "ETH": "0x5300000000000000000000000000000000000004",
+    "WETH": "0x5300000000000000000000000000000000000004",
+    "USDC": "0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4"
+}
