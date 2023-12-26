8d7531f1daff514aa7c6ebe5139c5d1717da58f6
Klondike <147417684+MyKlondike@users.noreply.github.com>
2023-12-26 16:34:19 +0500

    Update modules_settings.py

--- modules_settings.py
+++ modules_settings.py
@@ -1,6 +1,12 @@
 import asyncio
+import time
+
 from modules import *
 
 async def nft_origins(account_id, key):
     nft = NftOrigins(account_id, key)
     await nft.mint()
+
+async def check_nft_origins(account_id, key):
+    check = NftOrigins(account_id, key)
+    await check.checker_nft()
